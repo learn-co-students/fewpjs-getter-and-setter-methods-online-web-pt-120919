06ebd1f0801386438fe08c422011e9562407a5a3
github username <github email address>
2020-07-09 15:40:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your Circle class here
+
+class Circle {
+    constructor(radius) {
+        this.radius = radius 
+    }
+
+    get diameter() {
+        return this.radius * 2
+    }
+
+    get circumference() {
+        return this.diameter * Math.PI
+    }
+
+    get area() {
+        return Math.PI * this.radius ** 2
+    }
+
+    set diameter(diameter) {
+        this.radius = diameter / 2
+    }
+
+    set circumference(circumference) {
+        this.radius = circumference / (2 * Math.PI)
+    }
+
+    // The reason is that the only attribute our object has is radius - 
+    // everything else is calculated based on that attribute
+    // So, if we want to "set" the diameter, given that our only attribute 
+    // is radius, then we need to divide the given diameter by two to get 
+    // the radius and save it in the radius attribute
+}
